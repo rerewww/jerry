890f0123482766a85b4bc21f5134bbddfe49b2af
rerewww <rerewww@naver.com>
2019-04-30 23:37:50 +0900

    master/-/Add test class

--- src/test/java/com/jerry/service/AuthServiceTest.java
+++ src/test/java/com/jerry/service/AuthServiceTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpSession;
 
+import static org.mockito.Mockito.doReturn;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 /**
  * Created by son on 2019-04-07.
  */
@@ -15,8 +19,10 @@ public class AuthServiceTest {
 	private AuthService service;
 
 	@Mock private ServerConfig config;
+	@Mock private MockHttpSession session;
 
-	private String TEST_USER_ID = "TEST_USER_ID";
+	private static final String TEST_USER_ID = "TEST_USER_ID";
+
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
@@ -26,5 +32,18 @@ public class AuthServiceTest {
 	@Test
 	public void createUniqueKey() {
 		System.out.println(service.createUniqueKey(TEST_USER_ID));
+	}
+
+	@Test
+	public void checkAuthTest() {
+		doReturn("userId").doReturn("userId2").when(config).getAdminUserId();
+		doReturn("password").when(config).getAdminUserPassword();
+		assertThat(service.checkAuth("userId","password"), is(true));
+		assertThat(service.checkAuth("userId","password"), is(false));
+	}
+
+	@Test
+	public void checkSessionTest() {
+		assertThat(service.checkSession("userId", "userId"), is(true));
 	}
 }
