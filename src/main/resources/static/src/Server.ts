6ad8154b090ad28677fd0ea1308311392d6ffa88
rerewww <rerewww@naver.com>
2019-05-28 23:57:48 +0900

    master-task/#55/set typescript framework
    - 타입스크립트 설정 마무리

--- src/main/resources/static/src/Server.ts
+++ src/main/resources/static/src/Server.ts
@@ -6,83 +6,82 @@ import {Renderer} from "./Renderer";
 /**
  * Created by son on 2019-03-05.
  */
+const renderer = new Renderer();
 
 export class Server {
-    private renderer = new Renderer();
-
     public connect(): void {
         const sock = new SockJS(window.location.origin + '/stomp');
         const stompClient = Stomp.over(sock);
         Stomp.over(stompClient.connect({}, function(){
-            stompClient.subscribe('/subscribe/logs', this.onMessageForLogs);
-            stompClient.subscribe('/subscribe/accessLogs', this.onMessageForAccessLogs);
-            stompClient.subscribe('/subscribe/errorLogs', this.onMessageForErrorLogs);
-            stompClient.subscribe('/subscribe/usage', this.onMessageForUsage);
+            stompClient.subscribe('/subscribe/logs', onMessageForLogs);
+            stompClient.subscribe('/subscribe/accessLogs', onMessageForAccessLogs);
+            stompClient.subscribe('/subscribe/errorLogs', onMessageForErrorLogs);
+            stompClient.subscribe('/subscribe/usage', onMessageForUsage);
             stompClient.debug = null;
         }));
     }
+}
 
-    public onMessageForLogs(message): void {
-        if (message.body === "" || !clientConfig.checkedLog()) {
-            return;
-        }
-        this.renderer.drawLog(message.body);
+function onMessageForLogs(message): void {
+    if (message.body === "" || !clientConfig.checkedLog()) {
+        return;
+    }
+    renderer.drawLog(message.body);
+}
+
+function onMessageForAccessLogs(message): void {
+    if (message.body === "" || !clientConfig.checkedAccess()) {
+        return;
     }
 
-    public onMessageForAccessLogs(message): void {
-        if (message.body === "" || !clientConfig.checkedAccess()) {
-            return;
-        }
+    var oData = JSON.parse(message.body);
+    renderer.drawAccessLogs(oData);
+    if (!!oData && oData.accessLogs.length === 0) {
+        return;
+    }
 
-        var oData = JSON.parse(message.body);
-        this.renderer.drawAccessLogs(oData);
-        if (!!oData && oData.accessLogs.length === 0) {
-            return;
-        }
+// var date = moment().add(0, 'd').format();
+    var date = 0;
+    var successCnt = 0;
+    var failCnt = 0;
+    oData.accessLogs.forEach(function (item) {
+        var aData = item.split(' ');
+        var length = aData.length;
+        for (var i = 0; i < length; i++) {
+            var code = Number(aData.pop());
 
-        // var date = moment().add(0, 'd').format();
-        var date = 0;
-        var successCnt = 0;
-        var failCnt = 0;
-        oData.accessLogs.forEach(function (item) {
-            var aData = item.split(' ');
-            var length = aData.length;
-            for (var i = 0; i < length; i++) {
-                var code = Number(aData.pop());
-
-                if (!!code && (code >= 200 && code <= 308)) {
-                    successCnt++;
-                    break;
-                }
-                if (!!code && (code >= 400 && code <= 599)) {
-                    failCnt++;
-                    break;
-                }
+            if (!!code && (code >= 200 && code <= 308)) {
+                successCnt++;
+                break;
             }
-        });
-
-        var oSuccess = {
-            x: date,
-            y: successCnt
-        };
-        var oFail = {
-            x: date,
-            y: failCnt
-        };
-        this.renderer.updateChart(oSuccess, oFail);
-    }
-
-    public onMessageForErrorLogs(message): void {
-        if (message.body === "" || !clientConfig.checkedError()) {
-            return;
+            if (!!code && (code >= 400 && code <= 599)) {
+                failCnt++;
+                break;
+            }
         }
-        this.renderer.drawErrorElem(JSON.parse(message.body));
-    }
+    });
 
-    public onMessageForUsage(message): void {
-        if (message.body === "") {
-            return;
-        }
-        this.renderer.drawUsage(JSON.parse(message.body));
+    var oSuccess = {
+        x: date,
+        y: successCnt
+    };
+    var oFail = {
+        x: date,
+        y: failCnt
+    };
+    renderer.updateChart(oSuccess, oFail);
+}
+
+function onMessageForErrorLogs(message): void {
+    if (message.body === "" || !clientConfig.checkedError()) {
+        return;
     }
+    renderer.drawErrorElem(JSON.parse(message.body));
+}
+
+function onMessageForUsage(message): void {
+    if (message.body === "") {
+        return;
+    }
+    renderer.drawUsage(JSON.parse(message.body));
 }
