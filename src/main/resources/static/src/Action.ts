46dc9401691e228ef9407a3371e1660807803d13
rerewww <rerewww@naver.com>
2019-06-02 00:37:45 +0900

    master-task/#61/Action.js -> 타입스크립트로 변경
    - Action.js -> .ts 변경으로 인한 코드 수정
    - 사용하지 않는 파일 제거

---
+++ src/main/resources/static/src/Action.ts
@@ -0,0 +1,65 @@
+/**
+ * Created by son on 2019-03-09.
+ */
+import $ from 'jquery';
+export class action {
+	public setDefaultEnvs(data: any): void {
+		for (var i in data) {
+			window['_info'].defaultEnvs[i] = data[i];
+		}
+	}
+
+	public reset(): void {
+		const data = window['_info'].defaultEnvs;
+		const table = document.getElementById('settingTable') ;
+		for (var i =  0; i < table.childElementCount; i++) {
+			if (table.children[i].lastElementChild.nodeName === "TD") {
+				continue;
+			}
+			$(table.children[i].lastElementChild).text = data[table.children[i].firstChild.textContent];
+		}
+	}
+
+	public apply(): void {
+		const data = {};
+		const table = document.getElementById('settingTable');
+		for (var i = 0; i < table.childElementCount; i++) {
+			if (table.children[i].lastElementChild.nodeName === "TD") {
+				continue;
+			}
+			data[table.children[i].firstChild.textContent] = $(table.children[i].lastElementChild).text;
+		}
+		$.ajax({
+			url: 'apply.son',
+			type: 'POST',
+			async: true,
+			contentType: "application/json;charset=UTF-8",
+			traditional: true,
+			data: JSON.stringify(data),
+			dataType: 'json',
+			success: function (response) {
+				// alert
+			}
+		})
+	}
+
+	static viewCode(fileName: string, line: string, range: number, successCallback: any): void {
+		if (!line) {
+			return;
+		}
+
+		$('#loading').css('display', 'block');
+		$.ajax({
+			url: '/viewCode.son',
+			type: 'GET',
+			async: true,
+			data: {
+				fileName: fileName,
+				line: line,
+				range: $('#codeRange').text
+			},
+			dataType: 'json',
+			success: successCallback
+		})
+	}
+}
