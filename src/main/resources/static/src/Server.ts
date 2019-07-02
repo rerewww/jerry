db9659801353b06641e8a33cb79631d3a7292abf
sonsangwoo <rerewwwme@gmail.com>
2019-07-02 12:49:14 +0900

    master/-/ space to tab

--- src/main/resources/static/src/Server.ts
+++ src/main/resources/static/src/Server.ts
@@ -10,45 +10,45 @@ import {Renderer} from "./Renderer";
 const renderer: Renderer = new Renderer();
 const clientConfig: ClientConfig = new ClientConfig();
 export class Server {
-    public connect(): void {
-        const sock = new SockJS(window.location.origin + '/stomp');
-        const stompClient = Stomp.over(sock);
-        Stomp.over(stompClient.connect({}, function(){
-            stompClient.subscribe('/subscribe/logs', onMessageForLogs);
-            stompClient.subscribe('/subscribe/accessLogs', onMessageForAccessLogs);
-            stompClient.subscribe('/subscribe/errorLogs', onMessageForErrorLogs);
-            stompClient.subscribe('/subscribe/usage', onMessageForUsage);
-            stompClient.debug = null;
-        }));
-    }
+	public connect(): void {
+		const sock = new SockJS(window.location.origin + '/stomp');
+		const stompClient = Stomp.over(sock);
+		Stomp.over(stompClient.connect({}, function(){
+			stompClient.subscribe('/subscribe/logs', onMessageForLogs);
+			stompClient.subscribe('/subscribe/accessLogs', onMessageForAccessLogs);
+			stompClient.subscribe('/subscribe/errorLogs', onMessageForErrorLogs);
+			stompClient.subscribe('/subscribe/usage', onMessageForUsage);
+			stompClient.debug = null;
+		}));
+	}
 }
 
 function onMessageForLogs(message): void {
-    if (message.body === "" || !clientConfig.checkedLog()) {
-        return;
-    }
-    renderer.renderLogs(message.body);
+	if (message.body === "" || !clientConfig.checkedLog()) {
+		return;
+	}
+	renderer.renderLogs(message.body);
 }
 
 function onMessageForAccessLogs(message): void {
-    if (message.body === "" || !clientConfig.checkedAccess()) {
-        return;
-    }
+	if (message.body === "" || !clientConfig.checkedAccess()) {
+		return;
+	}
 
-    var oData = JSON.parse(message.body);
-    renderer.drawAccessLogs(oData);
+	var oData = JSON.parse(message.body);
+	renderer.drawAccessLogs(oData);
 }
 
 function onMessageForErrorLogs(message): void {
-    if (message.body === "" || !clientConfig.checkedError()) {
-        return;
-    }
-    renderer.renderErrorLogs(JSON.parse(message.body));
+	if (message.body === "" || !clientConfig.checkedError()) {
+		return;
+	}
+	renderer.renderErrorLogs(JSON.parse(message.body));
 }
 
 function onMessageForUsage(message): void {
-    if (message.body === "") {
-        return;
-    }
-    renderer.renderUsage(JSON.parse(message.body));
+	if (message.body === "") {
+		return;
+	}
+	renderer.renderUsage(JSON.parse(message.body));
 }
